4f8c6f16168e339a26b59258d9ab16fa2fa4d363
Gabriele Pagnotta <gabriele.pagnotta1998@gmail.com>
2023-03-19 01:38:48 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿//definire una funzione che  presi due numeri  renda loro la maedia.
+
+/*int Media(int num1, int num2, int num3)
+{
+    int sum = (num1 + num2 + num3) / 3;
+    return sum;
+}
+var RisultatoMedia = Media(4,8,6);
+Console.WriteLine(RisultatoMedia);*/
+
+//definire una funzione che allochi memoria e ponga numeri casuali in un vettore di double.
+
+
+/*double random()
+{
+var random = new Random();
+var rnd = random.NextDouble();
+var RandomNumber = Math.Round(rnd, 2);
+    return RandomNumber;
+
+}
+
+void Allocation()
+{
+    var random = new Random();
+    var rnd = random.NextDouble();
+    double RandomNumber = Math.Round(rnd, 2);
+    double[] arrayallocato = new double[5];
+    int i = 0;
+    while (i < arrayallocato.Length)
+    {
+        var fine = arrayallocato[i] = RandomNumber + RandomNumber; 
+        i++;
+        Console.WriteLine(fine);
+    }
+
+    
+
+
+}
+Allocation();*/
+
